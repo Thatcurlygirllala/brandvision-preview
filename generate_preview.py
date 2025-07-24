b104d8d9896988941b0f6ce5f59767f4462592ae
automationok <testevmorbbis92@gmail.com>
2025-07-24 18:48:10 -0300

    fix: add CORS support to Flask app

--- generate_preview.py
+++ generate_preview.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import openai
 import os
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app) 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/generate', methods=['POST'])
